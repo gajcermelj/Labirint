a61aaa65799ab948ad56ef76ff3fdad87beff50e
gajcermelj <136834705+gajcermelj@users.noreply.github.com>
2026-02-06 13:52:21 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
-c.fillStyle = "red";
-c.fillRect(24,24,24,24);
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = "red";
+ctx.fillRect(24,24,24,24);
+
+
+
+var mazePathX=[];
+	
+var mazePathY=[];
+
+function draw(){
+	ctx.beginPath();
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 2;
+	for (let i = 0; i < mazePathX.length; i+=2) {
+		ctx.moveTo(mazePathX[i], mazePathY[i]);
+		ctx.lineTo(mazePathX[i+1], mazePathY[i+1]);
+	}
+	ctx.stroke();
+}
+draw();
